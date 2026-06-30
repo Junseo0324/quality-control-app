014848d1a1d70cbab698b96a2446a77dbe592d50
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2026-06-30 16:11:27 +0900

    feat: DoughInputField 컴포넌트 상태 관리 방식 개선
    - StatelessWidget에서 StatefulWidget으로 전환하여 내부적으로 TextEditingController를 관리하도록 변경
    - 외부에서 controller를 직접 주입받는 대신 initialValue와 onChanged 콜백을 사용하도록 인터페이스 수정
    - 외부 상태 변화(예: 초기화)에 대응하기 위해 didUpdateWidget에서 컨트롤러 값 동기화 로직 추가
    - 메모리 누수 방지를 위한 controller dispose 처리 추가

--- lib/presentation/components/dough_input_field.dart
+++ lib/presentation/components/dough_input_field.dart
@@ -3,17 +3,49 @@ import 'package:flutter/material.dart';
 import '../../core/ui/app_colors.dart';
 import '../../core/ui/app_text_styles.dart';
 
-class DoughInputField extends StatelessWidget {
+class DoughInputField extends StatefulWidget {
   final String label;
   final String hintText;
-  final TextEditingController? controller;
+  final String initialValue;
+  final ValueChanged<String>? onChanged;
 
   const DoughInputField({
     super.key,
     required this.label,
     required this.hintText,
-    this.controller,
+    this.initialValue = '',
+    this.onChanged,
   });
+
+  @override
+  State<DoughInputField> createState() => _DoughInputFieldState();
+}
+
+class _DoughInputFieldState extends State<DoughInputField> {
+  late final TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController(text: widget.initialValue);
+    _controller.addListener(() {
+      widget.onChanged?.call(_controller.text);
+    });
+  }
+
+  @override
+  void didUpdateWidget(covariant DoughInputField oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.initialValue != widget.initialValue && _controller.text != widget.initialValue) {
+      _controller.text = widget.initialValue;
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +53,7 @@ class DoughInputField extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
-          label,
+          widget.label,
           style: AppTextStyles.labelSm.copyWith(
             color: AppColors.warmGray,
             fontWeight: FontWeight.w600,
@@ -40,14 +72,14 @@ class DoughInputField extends StatelessWidget {
             children: [
               Expanded(
                 child: TextField(
-                  controller: controller,
+                  controller: _controller,
                   style: AppTextStyles.displaySm.copyWith(
                     color: AppColors.charcoal,
                     fontWeight: FontWeight.w700,
                   ),
                   keyboardType: TextInputType.number,
                   decoration: InputDecoration(
-                    hintText: hintText,
+                    hintText: widget.hintText,
                     hintStyle: AppTextStyles.displaySm.copyWith(
                       color: AppColors.charcoal.withOpacity(0.5),
                       fontWeight: FontWeight.w700,
