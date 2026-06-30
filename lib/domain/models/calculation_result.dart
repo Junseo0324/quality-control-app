a4bdc0638f6590a6ae45defae3dcb340d5fa822c
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2026-06-30 16:30:33 +0900

    refactor: CalculationResult 모델 구조 단순화

--- lib/domain/models/calculation_result.dart
+++ lib/domain/models/calculation_result.dart
@@ -1,23 +1,11 @@
-class CarryOverResult {
+class CalculationResult {
   final bool isFakeLogicApplied;
   final int recommendedYesterdayDefrost;
   final String message;
 
-  CarryOverResult({
+  CalculationResult({
     required this.isFakeLogicApplied,
     required this.recommendedYesterdayDefrost,
     required this.message,
-  });
-}
-
-class CalculationResult {
-  final String defrostTime;
-  final String disposalTime;
-  final CarryOverResult carryOverResult;
-
-  CalculationResult({
-    required this.defrostTime,
-    required this.disposalTime,
-    required this.carryOverResult,
   });
 }
